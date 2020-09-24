376989f93b8ee6230c5df512fd1c73ca6035f2eb
Simon de Haan <simon@praekelt.org>
2020-09-24 10:32:50 +0200

    seeing how badly gen_stage upgrade breaks things

--- mix.exs
+++ mix.exs
@@ -8,17 +8,18 @@ defmodule Faktory.Mixfile do
       app: :faktory_worker_ex,
       version: @version,
       elixir: ">= 1.5.0",
-      start_permanent: Mix.env == :prod,
+      start_permanent: Mix.env() == :prod,
       deps: deps(),
-      elixirc_paths: elixirc_paths(Mix.env),
+      elixirc_paths: elixirc_paths(Mix.env()),
       aliases: aliases(),
 
       # Hex
-      description: "Elixir worker for Faktory (successor of Sidekiq); async/background queue processing",
+      description:
+        "Elixir worker for Faktory (successor of Sidekiq); async/background queue processing",
       package: [
         maintainers: ["Christopher J. Bottaro"],
         licenses: ["GNU General Public License v3.0"],
-        links: %{"GitHub" => "https://github.com/cjbottaro/faktory_worker_ex"},
+        links: %{"GitHub" => "https://github.com/cjbottaro/faktory_worker_ex"}
       ],
 
       # Docs
@@ -26,9 +27,9 @@ defmodule Faktory.Mixfile do
         extras: [
           "README.md": [title: "README", name: "readme"],
           "CHANGELOG.md": [title: "CHANGELOG"],
-          "Architecture.md": [title: "Architecture"],
+          "Architecture.md": [title: "Architecture"]
         ],
-        main: "readme",
+        main: "readme"
       ]
     ]
   end
@@ -48,9 +49,9 @@ defmodule Faktory.Mixfile do
       {:jason, "~> 1.1"},
       {:poolboy, "~> 1.5"},
       {:socket, "~> 0.3"},
-      {:gen_stage, "~> 0.14"},
+      {:gen_stage, "~> 1.0"},
       {:ex_doc, "~> 0.19", only: :dev},
-      {:mox, "~> 0.3", only: :test},
+      {:mox, "~> 0.3", only: :test}
     ]
   end
 
